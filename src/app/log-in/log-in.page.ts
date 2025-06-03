5d85e18a8f61846a3c45fba8b739255d9b5b2494
psyconach <ignacio.jana95@gmail.com>
2025-06-02 21:30:16 -0400

    agregando funcionalidad al formulario del login

--- src/app/log-in/log-in.page.ts
+++ src/app/log-in/log-in.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-log-in',
@@ -6,13 +8,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.page.scss'],
   standalone: false,
 })
-export class LogInPage implements OnInit {
+export class LogInPage  {
 
-  
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private router: Router,
+              private alertController: AlertController
+  ) { }
 
-  ngOnInit() {
+   // Método para mostrar alerta de error
+   async mostrarAlerta(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: mensaje,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  // Función para validar el formato del email
+  validarEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expresión regular básica para validar email
+    return emailRegex.test(email);
+  }
+
+  login() {
+    // Verificar que el campo de correo no esté vacío
+    if (!this.email) {
+      this.mostrarAlerta('El campo de correo no puede estar vacío.');
+      return;
+    }
+
+    // Validar el formato del correo
+    if (!this.validarEmail(this.email)) {
+      this.mostrarAlerta('El formato del correo es inválido.');
+      return;
+    }
+
+    // Verificar que la contraseña no esté vacía
+    if (!this.password) {
+      this.mostrarAlerta('El campo de contraseña no puede estar vacío.');
+      return;
+    }
+
+    // Verificar que la contraseña tenga máximo 4 caracteres
+    if (this.password.length < 4) {
+      this.mostrarAlerta('La contraseña no puede tener más de 4 caracteres.');
+      return;
+    }
+
+    // Si todas las validaciones son correctas, navega a la página "home"
+     this.router.navigate(['/home'], { state: { user: this.email } });
   }
 
 }

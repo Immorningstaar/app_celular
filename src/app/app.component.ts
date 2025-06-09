6d50557866af0024ce821b4511a891e577116fec
psyconach <ignacio.jana95@gmail.com>
2025-06-09 17:51:57 -0400

    animacion del menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular'; 
 import { Router } from '@angular/router'
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
@@ -8,11 +9,25 @@ import { Router } from '@angular/router'
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: false,
+  animations: [
+  trigger('menuAnimation', [
+    state('open', style({
+      transform: 'translateX(0)'
+    })),
+    state('closed', style({
+      transform: 'translateX(-100%)'
+    })),
+    transition('open <=> closed', [
+      animate('300ms ease-in-out')
+    ])
+  ])
+]
 })
 export class AppComponent {
   constructor(
     private menu: MenuController,
-    private router: Router
+    private router: Router,
+    
   ) {}
 
   logout() {

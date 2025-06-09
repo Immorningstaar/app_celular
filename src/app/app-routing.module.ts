b1ad39113efa94894c2dbc723c6b61b43eb1b355
psyconach <ignacio.jana95@gmail.com>
2025-06-09 12:27:01 -0400

    ordenando paginas, en la carpeta paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'log-in',
-    loadChildren: () => import('./log-in/log-in.module').then( m => m.LogInPageModule)
+    loadChildren: () => import('./paginas/log-in/log-in.module').then( m => m.LogInPageModule)
   },
   {
     path: '',
@@ -13,14 +13,15 @@ const routes: Routes = [
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () => import('./paginas/perfil/perfil.module').then( m => m.PerfilPageModule)
   },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },  {
+  },
+  {
     path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./paginas/registro/registro.module').then( m => m.RegistroPageModule)
   },
 
 ];
